ef025c99e757d393f6504177e5b28c9a858190b0
scott.liao <shazi7804@gmail.com>
2018-02-08 23:19:39 +0800

    add backup to cifs example

---
+++ examples/backup_cifs.pp
@@ -0,0 +1,11 @@
+class { 'corp104_backup':
+  server        => '10.1.10.200',
+  server_target => '/backup',
+  auth_user     => 'backup_user',
+  auth_pass     => 'backup_pass',
+  local_target  => '/backup',
+}
+
+corp104_backup::cifs { 'default':
+  backup_list = [ '/tmp', '/var/tmp' ],
+}
